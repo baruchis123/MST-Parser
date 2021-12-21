0c682bd66e7f05d6e1b10242b64c656e68348d7c
baruch-susman <baruch.susman@mail.huji.ac.il>
2021-12-21 14:19:40 +0200

    In this commit we added 3 new additions: 1. Creation of POS vocab which holds all types of POS. 2. Creation of POS_dict which indexes a all POS as a one dimension vector. 3. Given 2 words, return a vector with 1 corresponding to their 2 POS else the coordinate is 0.

--- ex4.py
+++ ex4.py
@@ -3,10 +3,15 @@ from nltk.corpus import dependency_treebank
 
 sents = dependency_treebank.parsed_sents()
 words = dependency_treebank.sents()
+POS = dependency_treebank.tagged_sents()
 
 
 def create_vocab_arr():
     return np.array(sorted(set([word for sentence in dependency_treebank.sents() for word in sentence])))
+
+
+def create_POS_arr():
+    return np.array(sorted(set([word[1] for tagged_sentence in dependency_treebank.tagged_sents() for word in tagged_sentence])))
 
 
 class MST_Parser:
@@ -14,20 +19,32 @@ class MST_Parser:
         self.weights = np.zeros(num_of_features)
         self.learning_rate = learning_rate
         self.vocab = create_vocab_arr()
+        self.POS = create_POS_arr()
         self.bigram_dict = {word1: {word2: 0 for word2 in self.vocab} for word1 in self.vocab}
         i = 0
         for word1 in self.vocab:
             for word2 in self.vocab:
                 self.bigram_dict[word1][word2] = i
                 i += 1
+        self.POS_dict = {pos1: {pos2: 0 for pos2 in self.POS} for pos1 in self.POS}
+        j = 0
+        for pos1 in self.POS:
+            for pos2 in self.POS:
+                self.POS_dict[pos1][pos2] = j
+                j += 1
+
 
     def _feature_function_word_bigram(self, word1, word2):
         arr = np.zeros(len(self.vocab)**2)
-        arr[self.bigram_dict[word1][word2]] = 1
+        arr[self.bigram_dict[word1[0]][word2[0]]] = 1
+        arr[self.bigram_dict[word2[0]][word1[0]]] = 1
         return arr
 
     def _feature_function_POS_bigram(self, word1, word2):
-        return
+        arr = np.zeros(len(self.POS) ** 2)
+        arr[self.POS_dict[word1[1]][word2[1]]] = 1
+        arr[self.POS_dict[word2[1]][word1[1]]] = 1
+        return arr
 
     def concat_features(self, word1, word2):
         return np.concatenate((self._feature_function_word_bigram(word1, word2), self._feature_function_POS_bigram(word1, word2)), axis=0)
