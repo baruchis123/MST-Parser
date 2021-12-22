e1973707b8a6a2dff051b6937a186a2396a24074
baruch-susman <baruch.susman@mail.huji.ac.il>
2021-12-22 17:52:14 +0200

    With a function to find a pair index instead of a dictionary

--- ex4.py
+++ ex4.py
@@ -32,12 +32,17 @@ class MST_Parser:
         self.__POS = create_POS_arr()
         self.__dimension_size = len(self.__vocab) ** 2 + len(self.__POS) ** 2
         self.__weights = np.zeros(self.__dimension_size)
-        self.__bigram_dict = {word1: {word2: 0 for word2 in self.__vocab} for word1 in self.__vocab}
+        self.bigram_word_index_dict = {}
         i = 0
-        for word1 in self.__vocab: #TODO verify dim size of vocab
-            for word2 in self.__vocab:
-                self.__bigram_dict[word1][word2] = i
-                i += 1
+        for word in self.__vocab:
+            self.bigram_word_index_dict[word] = i
+            i += 1
+        # self.__bigram_dict = {word1: {word2: 0 for word2 in self.__vocab} for word1 in self.__vocab}
+        # i = 0
+        # for word1 in self.__vocab: #TODO verify dim size of vocab
+        #     for word2 in self.__vocab:
+        #         self.__bigram_dict[word1][word2] = i
+        #         i += 1
         self.__POS_dict = {pos1: {pos2: 0 for pos2 in self.__POS} for pos1 in self.__POS}
         j = 0
         for pos1 in self.__POS: #TODO verify dim size of POS dict
@@ -45,11 +50,14 @@ class MST_Parser:
                 self.__POS_dict[pos1][pos2] = j
                 j += 1
 
+    def find_pair_index(self, word1, word2):
+        return self.bigram_word_index_dict[word1]*len(self.__vocab) + self.bigram_word_index_dict[word2]
+
     def __feature_function_word_bigram(self, word1, word2):
         arr = np.zeros(len(self.__vocab) ** 2)
         first_word = word1["word"] if word1["word"] is not None else "ROOT"
         second_word = word2["word"] if word2["word"] is not None else "ROOT"
-        arr[self.__bigram_dict[first_word][second_word]] = 1
+        arr[self.find_pair_index(first_word, second_word)] = 1
         return arr
 
     def __feature_function_POS_bigram(self, word1, word2):
@@ -61,7 +69,8 @@ class MST_Parser:
         return np.concatenate((self.__feature_function_word_bigram(word1, word2), self.__feature_function_POS_bigram(word1, word2)), axis=0)
 
     def __score_two_words(self, v1, v2):
-        index_words = self.__bigram_dict[v1[0]][v2[0]]
+        # index_words = self.__bigram_dict[v1[0]][v2[0]]
+        index_words = self.find_pair_index(v1[0], v2[0])
         index_POS = self.__POS_dict[v1[1]][v2[1]]
         return self.__weights[index_words] + self.__weights[index_POS]
 
